d113c70afe003d9dc0ccfbe756aa24d3345a70c5
Chandru <vivivinu3796@gmail.com>
2026-07-03 13:01:39 +0530

    chore: configure MongoDB connection and environment setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 
-const app=express();
-const PORT =5000;
+dotenv.config();
 
-app.get("/",(req,res)=>{
-    res.send("Backend in running");
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
